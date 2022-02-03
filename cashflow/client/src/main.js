906671f3f768091c9730253f1f73d0471634fd65
vladkim91 <vkcoding21@gmail.com>
2022-02-03 08:15:46 -0800

    vue dependencies installed/style dir created/router,main added or modified

--- cashflow/client/src/main.js
+++ cashflow/client/src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import router from './router';
+import './styles/index.css';
+import './styles/App.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App)
+}).$mount('#app');
---
+++ cashflow/client/src/router.js
@@ -0,0 +1,8 @@
+import VueRouter from 'vue-router';
+
+// import Home from './components/Home.vue'
+
+export default new VueRouter({
+  routes,
+  mode: 'history'
+});
