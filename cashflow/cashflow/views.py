90e38de31ee364766b3886804e0691f79903fe4e
vladkim91 <vkcoding21@gmail.com>
2022-02-03 08:58:02 -0800

    views added

--- cashflow/cashflow/views.py
+++ cashflow/cashflow/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serizaliers import UserSerializer, InfluenceSerializer
+from .models import User, Influence
 
-# Create your views here.
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class InfluenceList(generics.ListCreateAPIView):
+    queryset = Influence.objects.all()
+    serializer_class = InfluenceSerializer
+
+
+class InfluenceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Influence.objects.all()
+    serializer_class = InfluenceSerializer
