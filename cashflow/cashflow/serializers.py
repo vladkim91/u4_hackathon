23e741917aad501c815e3dafd1cfa9826a092ab9
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-03 10:25:58 -0800

    Updated serializers

--- cashflow/cashflow/serializers.py
+++ cashflow/cashflow/serializers.py
@@ -4,12 +4,16 @@ from .models import User, Influence
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     bills = serializers.HyperlinkedRelatedField(
-        view_name='profile_detail',
-        many=True
+        view_name='influence_detail',
+        lookup_field='bills',
+        many=True,
+        read_only=True
     )
     balance_history = serializers.HyperlinkedRelatedField(
-        view_name='profile_detail',
-        many=True
+        view_name='influence_detail',
+        lookup_field='balance_history',
+        many=True,
+        read_only=True
     )
 
     class Meta:
@@ -20,9 +24,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 class InfluenceSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.HyperlinkedRelatedField(
-        view_name='profile_detail'
+        view_name='profile_detail',
+        read_only=True
     )
 
     class Meta:
         model = Influence
-        fields = ('name', 'amount', 'date')
+        fields = ('name', 'amount', 'date', 'user',)
