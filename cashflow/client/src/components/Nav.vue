45c0b0c855f8580515f25adeef114268e6aa9948
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-04 06:33:12 -0800

    Setup the logout button

--- cashflow/client/src/components/Nav.vue
+++ cashflow/client/src/components/Nav.vue
@@ -3,13 +3,21 @@
     <router-link to="/">Home</router-link>
     <router-link to="/resources">Recources</router-link>
     <router-link to="/about">About</router-link>
+    <button v-if="userIsLoggedIn()" @click="logout">Logout</button>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Nav'
-
-  
+  name: 'Nav',
+  methods: {
+    userIsLoggedIn(){
+      return localStorage.getItem('userId') !== null
+    },
+    logout(){
+      localStorage.removeItem('userId')
+      this.$router.push('/login')
+    }
+  }
 };
 </script>
