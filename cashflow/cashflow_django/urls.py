324cdf14b1ab969ea4ae4801f121454c642a1e0c
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-03 18:01:19 -0800

    Setup cors and boilerplate for vue services

--- cashflow/cashflow_django/urls.py
+++ cashflow/cashflow_django/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cashflow.urls')),
+    path('api/', include('cashflow.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- cashflow/client/src/services/LoginServices.js
+++ cashflow/client/src/services/LoginServices.js
@@ -1,6 +1,11 @@
 import Client from './';
 
 export const registerUser = async (userInfo) => {
-  const result = await Client.post('/profile/create', userInfo);
+  const result = await Client.post('/profile/create', userInfo, {
+    auth: {
+      username: 'cashflowuser',
+      password: 'cashflow'
+    }
+  });
   return result.data;
 };
